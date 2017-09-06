acf4ee33f17d31eb10dafc0525d4bbbe0563418a
gugry <gugry@mail.ru>
2017-09-07 08:55:17 +1000

    Creating project

---
+++ working_examples/working_examples/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for working_examples project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "working_examples.settings")
+
+application = get_wsgi_application()
